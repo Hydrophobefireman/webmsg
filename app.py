bd6ad6d838fd16c9d5d1f8f3a1ae2c630adc3208
me <bhavesh.pareek36@gmail.com>
2018-11-05 17:47:48 +0530

    fix

--- app.py
+++ app.py
@@ -822,8 +822,9 @@ def alter_chat_data(data, new_message=False, read=False, rstats=False):
 
 
 # for heroku nginx
-
-open("/tmp/app-initialized", "w").close()
+@app.before_serving
+def open_to_nginx():
+    open("/tmp/app-initialized", "w").close()
 
 
 if __name__ == "__main__":
