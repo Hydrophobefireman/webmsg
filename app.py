a265f3561d28fcaccf597ab644f7839b013b9efb
me <bhavesh.pareek36@gmail.com>
2018-12-24 15:40:07 +0530

    rename route

--- app.py
+++ app.py
@@ -236,7 +236,7 @@ async def get_previous_chats():
     )
 
 
-@app.route("/api/validate-chat", methods=["POST"], strict_slashes=False)
+@app.route("/api/chat-stats", methods=["POST"], strict_slashes=False)
 async def get_chat_ids():
     content_type = "application/octet-stream"
     form = await request.form
